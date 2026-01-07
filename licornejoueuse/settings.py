ac6ff4703e847d68b48b4d66279e9b9b0544ea24
Julien Delaborde <delaborde.julien@gmail.com>
2026-01-07 08:07:38 +0100

    feat: base of design

--- licornejoueuse/settings.py
+++ licornejoueuse/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
 ]
+
+if DEBUG:
+    INSTALLED_APPS += ["django_browser_reload"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,6 +54,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG:
+    MIDDLEWARE += [
+            "django_browser_reload.middleware.BrowserReloadMiddleware",
+            ]
 
 ROOT_URLCONF = 'licornejoueuse.urls'
 
@@ -116,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+        BASE_DIR / "static",
+        ]
+
+TAILWIND_APP_NAME = "theme"
--- licornejoueuse/urls.py
+++ licornejoueuse/urls.py
@@ -22,4 +22,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("games/", include("games.urls")),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
