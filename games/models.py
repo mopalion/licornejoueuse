c3879ba236933bda2ca92ca4f5c293cb1153fd65
Julien Delaborde <delaborde.julien@gmail.com>
2026-01-18 08:38:08 +0100

    feat: improve games model

--- games/models.py
+++ games/models.py
@@ -6,20 +6,67 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Illustrator(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Mechanism(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Label(models.Model):
+    label = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.label}"
+
 class Game(models.Model):
-    title = models.CharField(max_length=200, blank=False)
+    name = models.CharField(max_length=200, blank=False)
+    games_library_categorization = models.CharField(max_length=10, blank=True)
+    adapted_games_library_categorization = models.CharField(max_length=10, blank=True)
+    time = models.CharField(max_length=10, blank=True)
+    players_number = models.CharField(max_length=10, blank=True)
     details = models.TextField(blank=True)
-    add_date = models.DateTimeField("date added")
+    add_date = models.DateField("date added", blank=True, null=True)
     number = models.PositiveIntegerField("Number", blank=False, unique=True)
-    location = models.ForeignKey(Location, on_delete=models.PROTECT, blank=False)
+    price = models.PositiveIntegerField(blank=True, null=True)
+    year = models.PositiveIntegerField(blank=True, null=True)
+    trictrac_link = models.CharField(max_length=200, blank=True)
+    cards_number = models.PositiveIntegerField(blank=True,null=True)
+    cards_size = models.CharField(max_length=10, blank=True)
+    origin = models.CharField(max_length=10, blank=True)
     qrcode = models.ImageField(upload_to="medias/qrcode", blank=True)
     image = models.ImageField(upload_to="medias/games_images", blank=True)
     state = models.CharField(max_length=20, blank=True)
-    age = models.PositiveIntegerField()
+    age = models.CharField(max_length=10)
     theme = models.CharField(max_length=30, blank=True) 
     token = models.PositiveIntegerField(blank=False)
-    to_sold = models.BooleanField(default=False)
     game_type = models.CharField(max_length=20, default="boardgame", choices=[("boardgame","jds"), ("rpg","jdr"), ("wooden", "bois"), ("toys", "jouet")])
+    editor = models.CharField(max_length=100, blank=True)
+    last_inventory_date = models.DateField("last inventory date", blank=True, null=True)
+    rules_video_link = models.CharField(max_length=100, blank=True)
+
+
+    location = models.ForeignKey(Location, on_delete=models.PROTECT, blank=False)
+    illustrators = models.ManyToManyField(Illustrator, blank=True)
+    authors = models.ManyToManyField(Author, blank=True)
+    mechanisms = models.ManyToManyField(Mechanism, blank=True)
+    labels = models.ManyToManyField(Label, blank=True)
 
     def __str__(self):
-        return f"{self.title}({self.number})"
+        return f"{self.name}({self.number})"
+
+class Comment(models.Model):
+    text = models.TextField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
