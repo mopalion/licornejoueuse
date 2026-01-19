729eb26136d1af4ae2b978e3faef5c8c3433e209
Julien Delaborde <delaborde.julien@gmail.com>
2026-01-19 19:55:43 +0100

    feat: add myludo to complete games infos

--- games/data.py
+++ games/data.py
@@ -1,11 +1,137 @@
 import csv
 from datetime import datetime, date
-from .models import Game, Location, Label, Author, Illustrator, Mechanism, Comment
+from .models import Game, Location, Label, Author, Illustrator, Mechanism, Comment, Editor
 import requests
 from os import makedirs
+from os import environ
 from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError
 import re
+from playwright.sync_api import sync_playwright
+import traceback
+from os import getenv
+from playwright._impl._errors import TimeoutError as PlaywrightTimeoutError
+
+def log_error_in_file(e):
+    django_errors_dir = getenv("DJANGO_ERRORS_DIR")
+    if django_errors_dir != '':
+        django_errors_dir = "./"
+    with open(f"{django_errors_dir}errors.txt", "a") as fd:
+        fd.write("******************************************************")
+        fd.write(traceback.format_exc())
+        fd.write(str(e))
+        fd.write("\n")
+        fd.write(str(type(e)))
+        fd.write("\n")
+
+class MyLudoBrowser():
+    def __init__(self):
+        self.play = sync_playwright().start()
+        self.browser = self.play.chromium.launch()
+        self.page = self.browser.new_page()
+        self.page.goto("https://www.myludo.fr/#!", wait_until="networkidle")
+        l = self.page.get_by_role("button", name="OK, accept all")
+        l.click()
+        self.page.set_default_timeout(1000)
+
+    def __del__(self):
+        self.browser.close()
+        self.play.stop()
+
+
+    def get_game_url(self, name):
+        self.page.goto(f"https://www.myludo.fr/#!/search/{name}", wait_until="networkidle")
+        try:
+            elem = self.page.locator("p.title.grey-text.text-darken-4 > a").first
+            link = elem.get_attribute("href")
+        except PlaywrightTimeoutError:
+            return None
+        return link
+
+
+    def get_game_data(self, game_number, check_game=True):
+        game = Game.objects.filter(number=game_number)[0]
+        if check_game:
+            if game.myludo_path is not None and game.myludo_path != "":
+                link = game.myludo_path
+            else:
+                link = self.get_game_url(game.name)
+        else:
+            link = self.get_game_url(game.name)
+        if link is None:
+            return None
+        myludo_url = f"https://www.myludo.fr/{link}"
+        print(f"myludo_url: {myludo_url}")
+        self.page.goto(myludo_url, wait_until="networkidle")
+        self.page.wait_for_timeout(1000)
+
+        themes = []
+        for theme_markup in self.page.get_by_title("Thématique").all()[1:]:
+            themes.append(re.sub(r"local_offer", "", theme_markup.text_content()))
+        mechanisms = []
+        for mechanism_markup in self.page.get_by_title("Mécanisme").all():
+            mechanisms.append(re.sub(r"settings", "", mechanism_markup.text_content()))
+
+        descriptions = self.page.locator(".card-content > .hide-on-small-only > p").all()
+        description = ""
+        first = True
+        for paragraph in descriptions:
+            if not first:
+                description += "\n\n"
+            else:
+                first = False
+            description += f"{paragraph.text_content()}"
+
+        contents = []
+        for content in self.page.get_by_text("Contenu").locator("xpath=following-sibling::*[1]").locator("li").all():
+            contents.append(content.text_content().strip())
+
+        authors = []
+        for author in self.page.get_by_text("Auteur", exact=True).locator("xpath=preceding-sibling::p[1]/a").all():
+            authors.append(author.text_content())
+
+        illustrators = []
+        for illustrator in self.page.get_by_text("Illustrateur", exact=True).locator("xpath=preceding-sibling::p[1]/a").all():
+            illustrators.append(illustrator.text_content())
+
+        editors = []
+        try:
+            for editor in self.page.get_by_text("Éditeur", exact=True).locator("xpath=preceding-sibling::p[1]/a").all():
+                editors.append(editor.text_content())
+        except PlaywrightTimeoutError:
+            pass
+
+        self.page.screenshot(path="/mnt/c/Users/jujud/Documents/toto.png")
+
+
+        infos = {
+            "img": self.page.query_selector("picture img").get_attribute("src"),
+            "players": self.page.get_by_title("Joueurs").locator("xpath=following-sibling::*[1]").text_content(),
+            "age": self.page.get_by_title("Âge").locator("xpath=following-sibling::*[1]").text_content(),
+            "time": self.page.get_by_title("Durée").locator("xpath=following-sibling::*[1]").text_content(),
+            "themes": themes,
+            "mechanisms": mechanisms,
+            "description": description,
+            "release_year": self.page.locator("span.edition").text_content().strip(),
+            "contents": contents,
+            "authors": authors,
+            "illustrators": illustrators,
+            "myludo_path": link,
+            "editors": editors,
+        }
+
+
+        cards_size = []
+        try:
+             elements = self.page.locator("a.sleeve > p > strong").all()
+             for elem in elements:
+                 cards_size.append(elem.text_content())
+             infos["cards_size"] = ",".join(cards_size)
+        except PlaywrightTimeoutError:
+            pass
+
+        return infos
+
 
 def delete_games():
     games = Game.objects.all()
@@ -132,3 +258,72 @@ def load_data(csvfilename):
     print(f"Jeux en erreurs : {len(error_games)}")
     print(f"Pourcentage de réussite : {current_games*100/total_games}")
 
+def complete_data(label=None, remove_label = False):
+    environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+    if label is None:
+        uncomplete_games = Game.objects.all() 
+    else:
+        filtering_label = Label.objects.filter(label=label)[0]
+        uncomplete_games = filtering_label.game_set.all()
+    myludo = MyLudoBrowser()
+    for game in uncomplete_games:
+        print(f"début: {game}")
+        infos = myludo.get_game_data(game.number)
+        if infos is None:
+            print("go suivant")
+            continue
+        print("suite du début")
+
+        r = requests.get(infos["img"], allow_redirects=True)
+        makedirs("medias/games_images", exist_ok=True)
+        with open(f"medias/games_images/{game.name}.jpg", "wb") as fd:
+            fd.write(r.content)
+        game.image = f"medias/games_images/{game.name}.jpg"
+        game.players_number = infos["players"]
+        game.myludo_path = infos["myludo_path"]
+        game.age = infos["age"]
+        game.time = infos["time"]
+        game.theme = ",".join(infos["themes"])
+        for mechanism_name in infos["mechanisms"]:
+            mechanisms = Mechanism.objects.filter(name=mechanism_name)
+            if len(mechanisms) != 0:
+                mechanism = mechanisms[0]
+            else:
+                mechanism = Mechanism(name=mechanism_name)
+                mechanism.save()
+            game.mechanisms.add(mechanism)
+        game.details = infos["description"]
+
+        if game.year == "":
+            game.year = infos["release_year"].strip().split(" ")[0]
+        for editor_name in infos["editors"]:
+            editors = Editor.objects.filter(name=editor_name)
+            if len(editors) != 0:
+                editor = editors[0]
+            else:
+                editor = Editor(name=editor_name)
+                editor.save()
+            game.editors.add(editor)
+        for author_name in infos["authors"]:
+            authors = Author.objects.filter(name=author_name)
+            if len(authors) != 0:
+                author = authors[0]
+            else:
+                author = Author(name=author_name)
+                author.save()
+            game.authors.add(author)
+        for illustrator_name in infos["illustrators"]:
+            illustrators = Illustrator.objects.filter(name=illustrator_name)
+            if len(illustrators) != 0:
+                illustrator = illustrators[0]
+            else:
+                illustrator = Illustrator(name=illustrator_name)
+                illustrator.save()
+            game.illustrators.add(illustrator)
+
+
+
+
+        game.save()
+        if label is not None:
+            game.labels.remove(filtering_label)
--- games/models.py
+++ games/models.py
@@ -18,6 +18,12 @@ class Illustrator(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+class Editor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Mechanism(models.Model):
     name = models.CharField(max_length=50)
 
@@ -39,12 +45,12 @@ class Game(models.Model):
     details = models.TextField(blank=True)
     add_date = models.DateField("date added", blank=True, null=True)
     number = models.PositiveIntegerField("Number", blank=False, unique=True)
-    price = models.PositiveIntegerField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
     year = models.PositiveIntegerField(blank=True, null=True)
-    trictrac_link = models.CharField(max_length=200, blank=True)
+    myludo_path = models.CharField(max_length=200, blank=True, null=True)
     cards_number = models.PositiveIntegerField(blank=True,null=True)
-    cards_size = models.CharField(max_length=10, blank=True)
-    origin = models.CharField(max_length=10, blank=True)
+    cards_size = models.CharField(max_length=40, blank=True)
+    origin = models.CharField(max_length=30, blank=True)
     qrcode = models.ImageField(upload_to="medias/qrcode", blank=True)
     image = models.ImageField(upload_to="medias/games_images", blank=True)
     state = models.CharField(max_length=20, blank=True)
@@ -52,7 +58,6 @@ class Game(models.Model):
     theme = models.CharField(max_length=30, blank=True) 
     token = models.PositiveIntegerField(blank=False)
     game_type = models.CharField(max_length=20, default="boardgame", choices=[("boardgame","jds"), ("rpg","jdr"), ("wooden", "bois"), ("toys", "jouet")])
-    editor = models.CharField(max_length=100, blank=True)
     last_inventory_date = models.DateField("last inventory date", blank=True, null=True)
     rules_video_link = models.CharField(max_length=100, blank=True)
 
@@ -61,6 +66,7 @@ class Game(models.Model):
     illustrators = models.ManyToManyField(Illustrator, blank=True)
     authors = models.ManyToManyField(Author, blank=True)
     mechanisms = models.ManyToManyField(Mechanism, blank=True)
+    editors = models.ManyToManyField(Editor, blank=True)
     labels = models.ManyToManyField(Label, blank=True)
 
     def __str__(self):
