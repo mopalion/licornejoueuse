772a0126b4d79445b94033d869716df5f987b275
Julien Delaborde <delaborde.julien@gmail.com>
2026-01-26 22:08:14 +0100

    feat: add authentication

--- licornejoueuse/urls.py
+++ licornejoueuse/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import include,path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("games/", include("games.urls")),
+    path("accounts/", LoginView.as_view(template_name="login.html", next_page="/games/"), name='login'),
+    path("accounts/logout", LogoutView.as_view(next_page="/games/"), name='logout'),
     path('admin/', admin.site.urls),
 ]
 
