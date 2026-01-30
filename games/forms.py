0ca24f851f86029cd8df3a1d39498039e396ce74
Julien Delaborde <delaborde.julien@gmail.com>
2026-01-30 14:04:14 +0100

    feat: select games to apply actions. add batch action after have selected games.

--- games/forms.py
+++ games/forms.py
@@ -1,4 +1,33 @@
 from django import forms
+from django.db.models import Min
+import datetime
+
+class UnicornTextInputWidget(forms.TextInput):
+    """
+    Define django TextInput widget with applied tailwind classes.
+    """
+    def __init__(self):
+        super().__init__(attrs={
+            "class": "text-zinc-800 input"
+        })
+
+class UnicornSelectWidget(forms.Select):
+    """
+    Define django Select widget with applied tailwind classes.
+    """
+    def __init__(self):
+        super().__init__(attrs={
+            "class": "text-zinc-800 select"
+        })
+
+class UnicornSelectMultipleWidget(forms.SelectMultiple):
+    """
+    Define django SelectMultiple widget with applied tailwind classes.
+    """
+    def __init__(self):
+        super().__init__(attrs={
+            "class": "text-zinc-800 select"
+        })
 
 class GameFilterForm(forms.Form):
     games_by_page = forms.CharField(
@@ -7,3 +36,36 @@ class GameFilterForm(forms.Form):
         widget=forms.TextInput(attrs={"class": "input"})
     )
     name = forms.CharField(label="Nom du jeu", max_length=200)
+
+class InventoryFilterForm(forms.Form):
+    """
+    Form to filter games in inventory selecting view.
+    """
+    def __init__(self, locations, labels, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"] = forms.CharField(max_length=100, required=False, label="Nom du jeu", widget=UnicornTextInputWidget)
+        self.fields["min_number"] = forms.IntegerField(min_value=1,max_value=2000, required=False, label="Numéro minimal du jeu", widget=UnicornTextInputWidget)
+        self.fields["max_number"] = forms.IntegerField(min_value=1,max_value=2000, required=False, label="Numéro maximal du jeu", widget=UnicornTextInputWidget)
+        locations_choices = [(x.name,x.name) for x in locations]
+        locations_choices.insert(0,("", "---"))
+        self.fields["location"] = forms.ChoiceField(required=False, label="Localisation", choices=locations_choices, widget=UnicornSelectWidget)
+        min_year = 2013
+        current_year = datetime.date.today().year
+        years = list(map(lambda x: (x,x),range(min_year,current_year)))
+        years.insert(0, ("", "---"))
+        self.fields["min_entry_year"] = forms.ChoiceField(choices=years, required=False, label="Année minimum", widget=UnicornSelectWidget)
+        self.fields["max_entry_year"] = forms.ChoiceField(choices=years, required=False, label="Année maximum", widget=UnicornSelectWidget)
+        labels_choices = [(x.label,x.label) for x in labels]
+        self.fields["labels"] = forms.MultipleChoiceField(choices=labels_choices, required=False, label="Labels", widget=UnicornSelectMultipleWidget)
+
+class BatchForm(forms.Form):
+    """
+    Form to change attributes in batch of selected games.
+    """
+    def __init__(self, locations, labels, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        labels_choices = [(x.label,x.label) for x in labels]
+        self.fields["labels"] = forms.MultipleChoiceField(choices=labels_choices, required=False, label="Labels", widget=UnicornSelectMultipleWidget)
+        locations = [(x.name,x.name) for x in locations]
+        locations.insert(0,("", "---"))
+        self.fields["location"] = forms.ChoiceField(required=False, label="Localisation", choices=locations, widget=UnicornSelectWidget)
--- games/views.py
+++ games/views.py
@@ -3,12 +3,13 @@ from django.core.paginator import Paginator
 from django.utils.text import slugify
 from django.db.utils import IntegrityError
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 import segno
-from .forms import GameFilterForm
+from .forms import GameFilterForm,InventoryFilterForm, BatchForm
 from os import makedirs
 import requests
 
-from .models import Game,Location, Comment
+from .models import Game,Location, Comment, Label
 
 def generate_parameters(parameters):
     get_parameters = ""
@@ -135,3 +136,128 @@ def generate_comment_csv(request):
         } 
     )
     return response
+
+@login_required
+def inventory_index(request):
+    """
+    Root request to actions or selecting games views.
+
+    Args:
+        request: Django Request object.
+    Returns:
+        result of rooted view
+    """
+    if request.method == "POST":
+        if "actions" in request.POST and request.POST["actions"]:
+            match request.POST["actions"]:
+                case "batch":
+                    return batch(request)
+    return select_games(request)
+
+@login_required
+def batch(request):
+    """
+    Modify selected games in batch.
+
+    Args:
+        request: Django Request object.
+    Returns:
+        the form to select attributes to change or redirect to inventory page if attributes was modified.
+    """
+    form = BatchForm(Location.objects.all(), Label.objects.all())
+    selected_games = []
+    for selected_game in filter(lambda x: x[0].startswith("selected-game-") and "on" in x[1], request.POST.items()):
+        selected_games.append(int(selected_game[0].replace("selected-game-","")))
+    if selected_games == []:
+        return redirect("inventory_index")
+    games = Game.objects.filter(number__in=selected_games)
+    if "go" in request.POST:
+        for game in games:
+            if "labels" in request.POST and request.POST["labels"]:
+                for label_name in request.POST.getlist("labels"):
+                    label = Label.objects.filter(label=label_name)[0]
+                    game.labels.add(label)
+            if "location" in request.POST and request.POST["location"]:
+                location = Location.objects.get(name=request.POST["location"])
+                game.location = location
+                game.save()
+        return redirect("inventory_index")
+    context = {
+        "selected_games": games,
+        "selected_games_ids": map(lambda x: x.number, games),
+        "form": form,
+    }
+    return render(request, "games/batch.html", context)
+    
+def select_games(request):
+    """
+    Allow user to select games.
+
+    Args:
+        request: Django Request object.
+    Returns:
+        the form to select games.
+    """
+    games = None
+    selected_games = set()
+    current_selected_games = set()
+    if request.method == "POST":
+
+
+        for selected_game in filter(lambda x: x[0].startswith("selected-game-") and "on" in x[1], request.POST.items()):
+            selected_games.add(int(selected_game[0].replace("selected-game-","")))
+
+        form = InventoryFilterForm(Location.objects.all(), Label.objects.all(), request.POST)
+        if form.is_valid():
+            # Apply differents filters to games.
+            if "name" in form.cleaned_data and form.cleaned_data["name"]:
+                if games is None:
+                    games = Game.objects
+                games = games.filter(name__icontains=form.cleaned_data["name"])
+            if "min_number" in form.cleaned_data and form.cleaned_data["min_number"]:
+                if games is None:
+                    games = Game.objects
+                games = games.filter(number__gte=form.cleaned_data["min_number"])
+            if "max_number" in form.cleaned_data and form.cleaned_data["max_number"]:
+                if games is None:
+                    games = Game.objects
+                games = games.filter(number__lte=form.cleaned_data["max_number"])
+            if "location" in form.cleaned_data and form.cleaned_data["location"]:
+                if games is None:
+                    games = Game.objects
+                games = games.filter(location__name=form.cleaned_data["location"])
+            if "min_entry_year" in form.cleaned_data and form.cleaned_data["min_entry_year"]:
+                if games is None:
+                    games = Game.objects
+                games = games.filter(entry_year__gte=form.cleaned_data["min_entry_year"])
+            if "max_entry_year" in form.cleaned_data and form.cleaned_data["max_entry_year"]:
+                if games is None:
+                    games = Game.objects
+                games = games.filter(entry_year__lte=form.cleaned_data["max_entry_year"])
+            if "labels" in form.cleaned_data and form.cleaned_data["labels"]:
+                if games is None:
+                    games = Game.objects
+                for label in form.cleaned_data["labels"]:
+                    games = games.filter(labels__label=label)
+
+    else:
+        form = InventoryFilterForm(Location.objects.all(), Label.objects.all())
+
+    if games is None:
+        games = Game.objects.all()
+
+    # Allow to save selected games in previous filters.
+    for game in games:
+        if game.number in selected_games:
+            current_selected_games.add(game.number)
+            selected_games.remove(game.number)
+    selected_games_to_display = Game.objects.filter(number__in=selected_games)
+
+    context = {
+        "games": games,
+        "filter_form": form,
+        "selected_games": selected_games,
+        "selected_games_to_display": selected_games_to_display,
+        "current_selected_games": current_selected_games,
+    }
+    return render(request, "games/inventory_index.html", context)
