f5246fbacd3fae3ec9b6faa7148ef29031d3dc8f
Julien Delaborde <delaborde.julien@gmail.com>
2026-02-07 11:57:51 +0100

    feat: allow entry_year to be null in db for old games

--- games/models.py
+++ games/models.py
@@ -89,7 +89,7 @@ class Game(models.Model):
     time = models.CharField(max_length=10, blank=True)
     players_number = models.CharField(max_length=10, blank=True)
     details = models.TextField(blank=True)
-    entry_year = models.PositiveIntegerField(blank=False, null=False)
+    entry_year = models.PositiveIntegerField(blank=False, null=True)
     number = models.PositiveIntegerField("Number", blank=False, unique=True)
     price = models.FloatField(blank=True, null=True)
     year = models.PositiveIntegerField(blank=True, null=True)
